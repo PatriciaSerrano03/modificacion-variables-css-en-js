1ca10c69d6c7ff9bc402648454532b282af42d44
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-06-25 11:57:55 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+/* const rootStyles = document.documentElement.style;
+
+rootStyles.setProperty('--primary-color', 'green'); */
+
+//Crea un div de color rojo y colócalo en la parte superior izquierda de tu web. Haz que tu web tenga un min-height de 500vh para generar scroll. El div deberá sincronizarse con el scroll, si estás arriba del todo medirá 0 de ancho y si está abajo del todo medirá el 100%, según vayas haciendo scroll el div deberá ir creciendo o encogiendo en función de si subes o bajas.
+
+console.log(window);
